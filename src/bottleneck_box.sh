c3e0d4e4af87ebcb64d1c3c0ffbfd066f32f04fe
sekaiwazankoku <44140828+sekaiwazankoku@users.noreply.github.com>
2024-10-07 21:15:50 -0400

    Update bottleneck_box.sh

--- src/bottleneck_box.sh
+++ src/bottleneck_box.sh
@@ -18,43 +18,43 @@ DURATION=60  # seconds
 SCRIPT=$(realpath "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
-if [[ $tbf_size_bytes == false ]]; then
-    # CBR Box with finite buffer
-    if [[ $log_uplink == true ]]; then
-        mm-link \
-            $cbr_uplink_trace_file \
-            $downlink_trace_file \
-            --uplink-queue=droptail \
-            --uplink-log="$uplink_log_path" \
-            --uplink-queue-args="bytes=$buf_size_bytes" -- \
-            sh -c '$SCRIPT_PATH/sender.sh'
-    else
-        mm-link \
-            $cbr_uplink_trace_file \
-            $downlink_trace_file \
-            --uplink-queue=droptail \
-            --uplink-queue-args="bytes=$buf_size_bytes" -- \
-            sh -c '$SCRIPT_PATH/sender.sh'
-    fi
+
+# CBR Box with finite buffer
+if [[ $log_uplink == true ]]; then
+    mm-link \
+        $cbr_uplink_trace_file \
+        $downlink_trace_file \
+        --uplink-queue=droptail \
+        --uplink-log="$uplink_log_path" \
+        --uplink-queue-args="bytes=$buf_size_bytes" -- \
+        sh -c '$SCRIPT_PATH/sender.sh'
 else
-    # TBF Box with finite buffer
-    if [[ $log_uplink == true ]]; then
-        mm-tbf \
-            $pkts_per_ms $tbf_size_bytes \
-            $pkts_per_ms $tbf_size_bytes \
-            --uplink-queue=droptail \
-            --uplink-log="$uplink_log_path" \
-            --uplink-queue-args="bytes=$buf_size_bytes" -- \
-            sh -c '$SCRIPT_PATH/sender.sh'
-    else
-        mm-tbf \
-            $pkts_per_ms $tbf_size_bytes \
-            $pkts_per_ms $tbf_size_bytes \
-            --uplink-queue=droptail \
-            --uplink-queue-args="bytes=$buf_size_bytes" -- \
-            sh -c '$SCRIPT_PATH/sender.sh'
+    mm-link \
+        $cbr_uplink_trace_file \
+        $downlink_trace_file \
+        --uplink-queue=droptail \
+        --uplink-queue-args="bytes=$buf_size_bytes" -- \
+        sh -c '$SCRIPT_PATH/sender.sh'
     fi
-fi
+#else
+    # TBF Box with finite buffer
+ #   if [[ $log_uplink == true ]]; then
+ #       mm-tbf \
+  #          $pkts_per_ms $tbf_size_bytes \
+   #         $pkts_per_ms $tbf_size_bytes \
+    #        --uplink-queue=droptail \
+     #       --uplink-log="$uplink_log_path" \
+      #      --uplink-queue-args="bytes=$buf_size_bytes" -- \
+       #     sh -c '$SCRIPT_PATH/sender.sh'
+    #else
+     #   mm-tbf \
+      #      $pkts_per_ms $tbf_size_bytes \
+       #     $pkts_per_ms $tbf_size_bytes \
+        #    --uplink-queue=droptail \
+         #   --uplink-queue-args="bytes=$buf_size_bytes" -- \
+          #  sh -c '$SCRIPT_PATH/sender.sh'
+    #fi
+#fi
 
 if [[ $log_uplink == true ]]; then
     mv $uplink_log_path $DATA_PATH
